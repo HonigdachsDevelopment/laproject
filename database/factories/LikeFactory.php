596fe4a60d2063dcca48e14603f034c4fe9947cf
Kill Phil <honigdachs.development@gmail.com>
2020-06-12 10:07:26 +0200

    devbranch for basic project relatiomodels for forumn

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+use App\Model\Like;
+
+$factory->define(Like::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
